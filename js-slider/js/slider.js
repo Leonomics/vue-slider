10c1b8d40ecae9a2b18734b08f3b01a3a91f9fd2
Leonomics <leocv97@gmail.com>
2022-08-24 17:04:48 +0200

    add vue

---
+++ js-slider/js/slider.js
@@ -0,0 +1,43 @@
+// console.log('slider')
+
+const slides = [
+	slide1 = {
+		id:1,
+		media:'./img/01.jpg',
+	},
+	{
+		id:2,
+		media:'./img/02.jpg',
+	},
+	{
+		id:3,
+		media:'./img/03.jpg',
+	},
+	{
+		id:4,
+		media:'./img/04.jpg',
+	},
+	{
+		id:5,
+		media:'./img/05.jpg',
+	}
+]
+
+let currentSlideIndex = 0;
+
+const app = new Vue ({
+	el:'#root',
+	data:{
+		slides,
+		slideActive: 'active',
+	},
+	methods:{
+		onClick(){
+			slides.show = false
+		},
+		active(){
+			var slide = document.getElementById("slide");
+  			slide.classList.add("active");
+		},
+	}
+})
